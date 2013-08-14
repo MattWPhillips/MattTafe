1b1f7338e78ca1e0ddad242fcc91ac7dae7430ed
MattWPhillips <mantis_rulze@hotmail.com>
2013-08-15 09:35:13 +1000

    Update Form1.cs
    Changed Button 2 click to family name

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -20,7 +20,7 @@ namespace WindowsFormsApplication1
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string name = "Matt";
+            string name = "Phillips";
             MessageBox.Show(name);
         }
 
